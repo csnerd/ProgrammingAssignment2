cf944dd1d5b406f85e2ad1fc09cda9f76f098667
Mohamed <mohamed.abdelnaby@hotmail.com>
2020-08-29 10:24:03 +0200

    First Commit

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,21 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y){
+    x <<- y 
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  
+  list(set = set,
+       get = get,
+       setsolve = setsolve,
+       getsolve = setsolve)
+    
 }
 
 
@@ -12,4 +27,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m  <- x$getsolve()
+  if(!is.null(m)){
+    message("getting cached inverse")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setsolve(m)
+  m
 }
